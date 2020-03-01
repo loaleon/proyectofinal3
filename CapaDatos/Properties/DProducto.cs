bc27b02f5a2a2af93b6fab8d700a0d52b8748c93
teddy paulino <elvago56@hotmail.com>
2020-03-01 17:55:17 -0400

    Se crearon las capas de Entidad de Producto y la capa de Datos de producto

---
+++ CapaDatos/Properties/DProducto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using CapaEntidad;
+
+namespace CapaDatos.Properties
+{
+   public class DProducto
+    {
+        public void InsertarProducto(EProducto obj)
+        {
+
+            SqlConnection SqlCon = new SqlConnection();
+            SqlCon = Conexion.CrearInstancia().CrearConexion();
+            SqlCommand cmd = new SqlCommand("uspInsertProducto", SqlCon);
+
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@nombreProd", obj.idProducto);
+            cmd.Parameters.AddWithValue("@prodMax", obj.prodMax);
+            cmd.Parameters.AddWithValue("@prodMin", obj.prodMin);
+            cmd.Parameters.AddWithValue("@precioCompra", obj.precioVenta);
+            cmd.Parameters.AddWithValue("@precioVenta", obj.precioVenta);
+            cmd.Parameters.AddWithValue("@fechaIngreso", obj.fechaIngreso);
+            cmd.Parameters.AddWithValue("@idTipoProd", obj.idTipoProd);
+
+
+            SqlCon.Open();
+
+            cmd.ExecuteNonQuery();
+
+            SqlCon.Close();
+        }
+    }
+}
---
+++ CapaEntidad/EProducto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class EProducto
+    {
+        public int idProducto { get; set; }
+        public string nombreProd { get; set; }
+        public int prodMax { get; set; }
+        public int prodMin { get; set; }
+        public decimal precioCompra { get; set; }
+        public decimal precioVenta { get; set; }
+        public DateTime fechaIngreso { get; set; }
+        public int idTipoProd { get; set; }
+    }    
+}
